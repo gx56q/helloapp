78fc4f34980d1307c1da57ec1324aebd6f8f2f4f
black <black@example.com>
2022-10-23 16:46:38 +0500

    Initial commit

---
+++ helloapp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace helloapp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Hello();
+        }
+
+        private static void Hello()
+        {
+            Console.WriteLine("hello");
+        }
+    }
+}
